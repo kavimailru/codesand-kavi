2302dd10b348180f71075d033c8829f4043e50ec
kavimailru <54444389+kavimailru@users.noreply.github.com>
2021-12-11 00:33:23 +0300

    Lesson 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
 var myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+document.querySelector("h1").onclick = function () {
+  alert("Ouch! Stop poking me!");
+};
+var myButton = document.querySelector("button");
+
+function setUserName() {
+  var myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Mozilla is cool, " + myName;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+myButton.onclick = function () {
+  setUserName();
+};
